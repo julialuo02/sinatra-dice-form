cf750cb67478203e2dbf7e596f571fff31a55147
julialuo02 <121737951+julialuo02@users.noreply.github.com>
2025-01-27 23:17:12 +0000

    finished

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader' if development?
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get '/' do
+  erb :index
+end
+
+get '/process_roll' do
+  dice = params[:dice].to_i
+  sides = params[:sides].to_i
+  
+  if dice > 0 && sides > 0
+    @dice = dice
+    @sides = sides
+    @results = Array.new(dice) { rand(1..sides) }
+    erb :process_roll
+  else
+    @error = "Please enter valid positive integers for the number of dice and sides."
+    erb :error
+  end
 end
